a97adb38123ea63bca38c28b9bee25d18ebb82d2
Katie Martin <katieadalinemartin@gmail.com>
2020-01-19 17:20:44 -0500

    Initialize project using asp.net MVC

---
+++ src/YayGourmetProject/YayGourmetProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace YayGourmetProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
